7237a176c430763d10903f0773a2936f050bba77
Jeremy King <admin@jeremyking.co>
2024-09-22 18:48:37 -0400

    module 3 json static

--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var controller = require("../controllers/travel");
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync(`./data/trips.json`, `utf8`));
 
-router.get("/", controller.travel);
+const travel = (req, res) => {
+  res.render("travel", { title: "Travlr Getaways", trips });
+};
 
-module.exports = router;
+module.exports = travel;
