897586203c8c6dd5d4caa8591d869e4e63e57d56
Jeremy King <admin@jeremyking.co>
2024-10-21 23:19:33 -0400

    page 37

--- travlr/app.js
+++ travlr/app.js
@@ -28,11 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', (req, res, next)) => {
+app.use('/api', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-AllowHeaders', 'Origin, X-Requested-With , Content-Type, Accept');
     next();
-}
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
